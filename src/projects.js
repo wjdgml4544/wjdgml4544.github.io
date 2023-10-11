d17a4512c211d2dc615cee7c0217826da82963a5
wjdgml4544 <wjdgml4544@naver.com>
2023-10-11 21:56:50 +0900

    js-project

--- src/projects.js
+++ src/projects.js
@@ -1,4 +1,21 @@
 'use strict';
 
-const header = '안녕';
-console.log(header);
+// 프로젝트 필터링 관련 로직 처리
+const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('project');
+categories.addEventListener('click', (event) => {
+  const filter = event.target.dataset.category;
+  if(filter == null) {
+    return;
+  }
+  projects.forEach((project) => {
+    if (filter === 'all' || filter === project.dataset.type) {
+      project.style.display = 'block';
+    } else {
+      project.style.display = 'none';
+    }
+  });
+
+});
+
+
